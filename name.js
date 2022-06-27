4f78176b00fc4e862984f1bcb56ed7aec8ffc1f6
Sir Saviour B <sire.saviour@gmail.com>
2022-06-27 15:46:20 +0100

    Update Commit

---
+++ name.js
@@ -0,0 +1,8 @@
+//This Js program is to display my Name, height and Country on the Screen.
+// A submission to Zuri Team Training Intro to JavaScript Assignment. Task 86.
+//Code written by SirSaviour with Student ID- I4G0185511EH
+
+//console.log ('Saviour Prosper');
+document.write ("My name is Saviour Prosper B.");
+document.write ("My height is 1.95m.");
+document.write ("I am from Nigeria.");
